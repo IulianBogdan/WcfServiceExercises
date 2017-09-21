92e3def92fd3c16ed34cd37eb802e01af2df35dd
Bogdan-Iulian Vasile <bogd0268@edu.eal.dk>
2017-09-21 11:28:49 +0200

    Add project files.

---
+++ WCFexercises/CircleService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFexercises
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class CircleService : ICircleService
+    {
+        public const double PI = 3.1416;
+        public double CalcDiameter(double radius)
+        {
+            double result = 2 * radius;
+            return result;
+        }
+
+        public double CalcCircumference(double radius)
+        {
+            double result = 2 * PI * radius;
+            return result;
+        }
+    }
+}
---
+++ WcfServiceLibrary/Service1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public const double PI = 3.1416;
+       public const double EUR = 7.459900;
+
+        //public double ReturnExchangeRate(string ISO)
+        //{
+        //    double rate;
+        //    switch (ISO)
+        //    {
+        //        case "EUR":
+        //            rate = 7.44090;
+        //            break;
+        //        case "USD":
+        //            rate = 6.24976;
+        //            break;
+        //        case "GBR":
+        //            rate = 8.43565;
+        //            break;
+        //        default:
+        //            rate = 0;
+        //            break;
+
+        //    }
+        //    return rate;
+        //}
+
+        //public List<string> ListOfAvailableIso()
+        //{
+        //   List<string> list = new List<string>();
+        //    list.Add("USD");
+        //    list.Add("EUR");
+        //    list.Add("GBR");
+        //    return list;
+        //}
+        //public double ConvertToEUR(double amount)
+        //{
+        //    double result = EUR * amount;
+        //    return result;
+        //}
+        //public double CalcDiameter(double radius)
+        //{
+        //    double result = 2 * radius;
+        //    return result;
+        //}
+
+        //public double CalcCircumference(double radius)
+        //{
+        //    double result = 2 * PI * radius;
+        //    return result;
+        //}
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        public CurrencyObject[] Objects()
+        {
+            CurrencyObject[] array = new CurrencyObject[3];
+            array[0] = new CurrencyObject {Name = "American dollar", ISO = "USD", ExchangeRate = 6.24976};
+            array[1] = new CurrencyObject {Name = "British pound", ISO = "GBR", ExchangeRate = 8.43565};
+            array[2] = new CurrencyObject {Name = "Euro", ISO = "EUR", ExchangeRate = 7.44090};
+            return array;
+        }
+    }
+}
